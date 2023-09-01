f1157086a37f9136f219fbcbb8d5ca1af02349ad
ikede-divine-keno <ikededivine@gmail.com>
2023-08-31 18:10:57 -0700

    103-binary_tree_rotate_left.c

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Left-rotates a binary tree.
+ * @tree: A pointer to the root node of the tree to rotate.
+ *
+ * Return: A pointer to the new root node after rotation.
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *pvt, *tmp;
+
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+
+	pvt = tree->right;
+	tmp = pvt->left;
+	pvt->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pvt;
+	pvt->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pvt;
+		else
+			tmp->right = pvt;
+	}
+
+	return (pvt);
+}
