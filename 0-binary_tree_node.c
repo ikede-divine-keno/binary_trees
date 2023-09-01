c9d121475fe7989748dc23a00b50b27f34cce1c4
ikede-divine-keno <ikededivine@gmail.com>
2023-08-31 17:00:17 -0700

    1-binary_tree_insert_left.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_nodefunction that creates a binary tree node
+* binary_tree_node - function that creates a binary tree node
 * @parent:  function that creates a binary tree node
 * @value: is the value to put in the new node
 *
